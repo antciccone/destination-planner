37f4a01d9b077b767d06f7d99ada8559c21553e5
antciccone <aciccone@bu.edu>
2016-12-25 13:32:06 -0500

    weather by zip poro

--- app/models/ten_day_weather_zip.rb
+++ app/models/ten_day_weather_zip.rb
@@ -1,13 +1,12 @@
 class TenDayWeatherByZip
 
   def initialize(service={})
-    require "pry"; binding.pry
   end
 
-  def tendayforcast(zipcode)
+  def self.forcast(zipcode)
     WeatherService.new.ten_day_weather_by_zip(zipcode).map do |raw_data|
       TenDayWeatherByZip(raw_data)
-    end 
+    end
   end
 
 end
--- spec/models/ten_day_weather_zip_spec.rb
+++ spec/models/ten_day_weather_zip_spec.rb
@@ -4,8 +4,7 @@ describe 'TenDayWeatherByZip' do
   context '.tendayforcast' do
     it 'returns an array of weather forcast by zip' do
 
-      weather = TenDayWeatherByZip.tendayforcast(31049)
-
+      weather = TenDayWeatherByZip.forcast(31049)
       expect(weather.first).to respond_to(:high)
       expect(weather.first).to respond_to(:low)
       expect(weather.first.high.class).to eq(String)

4c96852d354a1ed09801027cfe526f23141ef5d3
antciccone <aciccone@bu.edu>
2016-12-26 10:04:12 -0500

    render json for all destinations

--- spec/requests/api/v1/destinations/all_destinations_request_spec.rb
+++ spec/requests/api/v1/destinations/all_destinations_request_spec.rb
@@ -4,14 +4,15 @@ require 'rails_helper'
 describe 'all destinations endpoint' do
   context 'api/v1/destinations' do
     it 'returns all destinations' do
-      Destination.create(name: "Sparta", zip: "07871", description: "childhood home", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
-      Destination.create(name: "Hells Kitchen", zip: "10036", description: "NYC district", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
+      destination1 = Destination.create(name: "Sparta", zip: "07871", description: "childhood home", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
+      destination2 = Destination.create(name: "Hells Kitchen", zip: "10036", description: "NYC district", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
 
-      get '/api/vi/destinations'
+      get "/api/v1/destinations"
 
-      destinations_json
+      destinations_json = JSON.parse(response.body)
 
-
+      expect(response).to be_success
+      expect(destinations_json.first['id']).to eq(destination1.id)
     end
   end
 end

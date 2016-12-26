6ff3d259d104e9016d86cf253a9686c4424e736d
antciccone <aciccone@bu.edu>
2016-12-26 10:18:16 -0500

    post for api

--- spec/requests/api/v1/destinations/all_destinations_request_spec.rb
+++ spec/requests/api/v1/destinations/all_destinations_request_spec.rb
@@ -18,15 +18,16 @@ describe 'all destinations endpoint' do
   end
 
   it 'returns a single destination' do
-    destination1 = Destination.create(name: "Sparta", zip: "07871", description: "childhood home", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
-    destination2 = Destination.create(name: "Hells Kitchen", zip: "10036", description: "NYC district", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
+      destination1 = Destination.create(name: "Sparta", zip: "07871", description: "childhood home", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
+      destination2 = Destination.create(name: "Hells Kitchen", zip: "10036", description: "NYC district", image_url: "http://www.scottolson.us/mohawk/lm12.jpg")
 
-    get "/api/v1/destinations/#{destination1.id}"
+      get "/api/v1/destinations/#{destination1.id}"
 
-    destination_json = JSON.parse(response.body)
+      destination_json = JSON.parse(response.body)
 
-    expect(response).to be_success
-    expect(destination_json['name']).to eq("Sparta")
-    expect(destination_json['zip']).to eq("07871")
+      expect(response).to be_success
+      expect(destination_json['name']).to eq("Sparta")
+      expect(destination_json['zip']).to eq("07871")
+
   end
 end
